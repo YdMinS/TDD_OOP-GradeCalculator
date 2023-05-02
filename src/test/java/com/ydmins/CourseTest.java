c5e4dc590d8206d5c9f660e9c0fa7471bdc47aa0
ydmins <dr.ydmins@gmail.com>
2023-05-02 12:44:47 +0900

    CourseTest, Course [생성]
    Course 도메인 객체 생성
    CourseTest 객체 생성 테스트 구현

---
+++ src/test/java/com/ydmins/CourseTest.java
@@ -0,0 +1,14 @@
+package com.ydmins;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class CourseTest {
+    @DisplayName("과목을 생성한다.")
+    @Test
+    void createTest() {
+        Assertions.assertThatCode(()-> new Course("OOP", 3, "A+"))
+                .doesNotThrowAnyException();
+    }
+}
